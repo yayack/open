b3d874cbe42ede2efdb90ad40cf8d15fc29a8192
yayack <55357975+yayack@users.noreply.github.com>
2020-04-22 00:25:15 +0800

    Add files via upload

---
+++ hiden.php
@@ -0,0 +1,20 @@
+<?php
+system('clear');
+system('rm -rf open');
+system('rm -rf main.py');
+system('rm -rf bot.txt');
+system('rm -rf bahan.txt');
+system('rm -rf no.txt');
+system('rm -rf cookie.txt');
+system('rm -rf BACA_TUTORIAL.txt');
+system('rm -rf session');
+system('rm -rf .link');
+system('rm -rf .history');
+system('rm -rf .c');
+$y = "\033[1;37m";
+$rr = "\033[0;33m";
+$r = "\033[0m";
+echo "----------[{$rr}SCRIPT_OFFLINE{$r}]----------\n";
+echo " {$y}Silahkan Tekan CTRL+C untuk Keluar{$r} \n";
+sleep(90000);
+?>
